03e103a1c0cd3c79d1be6b406d618e188fdb89c8
Juan Vicente Herrera Ruiz de Alejo <juan.vicente.herrera@gmail.com>
2013-08-09 16:36:58 +0200

    ada

--- cookbooks/wiki_app/recipes/db_master.rb
+++ cookbooks/wiki_app/recipes/db_master.rb
@@ -11,30 +11,52 @@ mysql_connection_info = {
   :username => "root",
   :password => mysql_secrets[node.chef_environment]['root']
 }
-
-# Create application database
-ruby_block "create_#{app_name}_db" do
-  block do
-    %x[mysql -uroot -p#{mysql_root_pass} -e "CREATE DATABASE #{node[app_name]['db_name']};"]
-  end 
-  not_if "mysql -uroot -p#{mysql_root_pass} -e \"SHOW DATABASES LIKE '#{node[app_name]['db_name']}'\" | grep #{node[app_name]['db_name']}";
+# Creamos la base de datos
+mysql_database '#{node[app_name]['db_name']}' do
+  connection mysql_connection_info
   action :create
 end
+
+
+# Create application database
+#ruby_block "create_#{app_name}_db" do
+#  block do
+#    %x[mysql -uroot -p#{mysql_root_pass} -e "CREATE DATABASE #{node[app_name]['db_name']};"]
+#  end 
+#  not_if "mysql -uroot -p#{mysql_root_pass} -e \"SHOW DATABASES LIKE '#{node[app_name]['db_name']}'\" | grep #{node[app_name]['db_name']}";
+#  action :create
+#end
 
 # Get a list of web servers
 webservers = node['roles'].include?('webserver') ? [{'ipaddress' => 'localhost'}] : search(:node, "role:webserver AND chef_environment:#{node.chef_environment}")
 
 # Grant mysql privileges for each web server 
+#webservers.each do |webserver|
+ # ip = webserver['ec2']['hostname']
+ # ruby_block "add_#{ip}_#{app_name}_permissions" do
+ #   block do
+  #    %x[mysql -u root -p#{mysql_root_pass} -e "GRANT SELECT,INSERT,UPDATE,DELETE \
+   #     ON #{node[app_name]['db_name']}.* TO '#{node[app_name]['db_user']}'@'#{ip}' IDENTIFIED BY '#{app_secrets[node.chef_environment]['db_pass']}';"]
+   # end
+   # not_if "mysql -u root -p#{mysql_root_pass} -e \"SELECT user, host FROM mysql.user\" | \
+   #   grep #{node[app_name]['db_user']} | grep #{ip}"
+   # action :create
+  #end
+#end
+
+
+
+
+
 webservers.each do |webserver|
   ip = webserver['ec2']['hostname']
-  ruby_block "add_#{ip}_#{app_name}_permissions" do
-    block do
-      %x[mysql -u root -p#{mysql_root_pass} -e "GRANT SELECT,INSERT,UPDATE,DELETE \
-        ON #{node[app_name]['db_name']}.* TO '#{node[app_name]['db_user']}'@'#{ip}' IDENTIFIED BY '#{app_secrets[node.chef_environment]['db_pass']}';"]
-    end
-    not_if "mysql -u root -p#{mysql_root_pass} -e \"SELECT user, host FROM mysql.user\" | \
-      grep #{node[app_name]['db_user']} | grep #{ip}"
-    action :create
+  mysql_database_user '#{node[app_name]['db_user']}' do
+  connection mysql_connection_info
+  database_name '#{node[app_name]['db_name']}'
+  host '#{ip}'
+  password '{app_secrets[node.chef_environment]['db_pass']}'
+  privileges [:all]
+  action :grant
   end
 end
 #mysql_database "#{node[app_name]['db_name']}" do
--- cookbooks/wiki_app/recipes/default.rb
+++ cookbooks/wiki_app/recipes/default.rb
@@ -6,7 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-include_recipe 'database::mysql'
+#include_recipe 'database::mysql'
 include_recipe "apache2"
 include_recipe "apache2::mod_php5"
 include_recipe "wiki_app::deploy"
