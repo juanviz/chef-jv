95cc7b24d642aa1d5ad72c206359cd24b82e4506
Juan Vicente Herrera Ruiz de Alejo <juan.vicente.herrera@gmail.com>
2013-08-12 11:21:52 +0200

    change public_ip for public hostname

--- cookbooks/wiki_app/recipes/webserver.rb
+++ cookbooks/wiki_app/recipes/webserver.rb
@@ -10,7 +10,7 @@ if node['roles'].include?('db')
   master_db_host = 'localhost'
 else
   results = search(:node, "role:db AND chef_environment:#{node.chef_environment}")
-  master_db_host = results[0]['ec2']['public_ipv4']
+  master_db_host = results[0]['ec2']['public_hostname']
 end 
 directory "#{app_config['config_dir']}" do
   owner "root"
